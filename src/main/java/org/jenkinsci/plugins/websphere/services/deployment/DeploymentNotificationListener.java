b73236490edb6a7da551521ce29a7d77a8bc2966
Greg Peters <gregpeters00@gmail.com>
2015-08-21 11:37:31 -0400

    updates

--- src/main/java/org/jenkinsci/plugins/websphere/services/deployment/DeploymentNotificationListener.java
+++ src/main/java/org/jenkinsci/plugins/websphere/services/deployment/DeploymentNotificationListener.java
@@ -3,6 +3,7 @@ package org.jenkinsci.plugins.websphere.services.deployment;
 import hudson.model.BuildListener;
 
 import java.util.Properties;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.management.Notification;

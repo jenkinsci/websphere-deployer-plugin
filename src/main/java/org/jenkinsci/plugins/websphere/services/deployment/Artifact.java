443f49d0f8cd8bcb06878ce48cbb8609fd8d380a
Greg Peters <gregpeters00@gmail.com>
2015-08-20 21:33:59 -0400

    added multiple target support. added multiple deployment options.

--- src/main/java/org/jenkinsci/plugins/websphere/services/deployment/Artifact.java
+++ src/main/java/org/jenkinsci/plugins/websphere/services/deployment/Artifact.java
@@ -10,10 +10,58 @@ public class Artifact {
     public static final int TYPE_RAR = 4;
     private File sourcePath;
     private String appName;
+    private String context;
+    private String targets;
     private int type;
+    private boolean distribute;
     private boolean precompile;
+    private boolean jspReloading;
+    private boolean reloading;
+    private String installPath;
+    private String classLoaderOrder;
+    private String classLoaderPolicy;
+    
+	public String getClassLoaderOrder() {
+		return classLoaderOrder;
+	}
 
-    public boolean isPrecompile() {
+	public void setClassLoaderOrder(String classLoaderOrder) {
+		this.classLoaderOrder = classLoaderOrder;
+	}
+
+	public String getClassLoaderPolicy() {
+		return classLoaderPolicy;
+	}
+
+	public void setClassLoaderPolicy(String classLoaderPolicy) {
+		this.classLoaderPolicy = classLoaderPolicy;
+	}
+
+	public boolean isReloading() {
+		return reloading;
+	}
+
+	public void setReloading(boolean reloading) {
+		this.reloading = reloading;
+	}
+
+	public String getTargets() {
+		return targets;
+	}
+
+	public void setTargets(String targets) {
+		this.targets = targets;
+	}
+
+	public boolean isJspReloading() {
+		return jspReloading;
+	}
+
+	public void setJspReloading(boolean jspReloading) {
+		this.jspReloading = jspReloading;
+	}
+
+	public boolean isPrecompile() {
         return precompile;
     }
 
@@ -44,4 +92,28 @@ public class Artifact {
     public void setSourcePath(File sourcePath) {
         this.sourcePath = sourcePath;
     }
+
+	public String getContext() {
+		return context;
+	}
+
+	public void setContext(String context) {
+		this.context = context;
+	}
+
+	public boolean isDistribute() {
+		return distribute;
+	}
+
+	public void setDistribute(boolean distribute) {
+		this.distribute = distribute;
+	}
+	
+	public String getInstallPath() {
+		return installPath;
+	}
+	
+	public void setInstallPath(String installPath) {
+		this.installPath = installPath;
+	}  		
 }

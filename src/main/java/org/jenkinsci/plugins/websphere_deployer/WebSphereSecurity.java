0225295990489b6d900ce18df910fb19fb10c57d
Greg Peters <gregpeters00@gmail.com>
2015-08-21 11:38:36 -0400

    added support for multiple targets

--- src/main/java/org/jenkinsci/plugins/websphere_deployer/WebSphereSecurity.java
+++ src/main/java/org/jenkinsci/plugins/websphere_deployer/WebSphereSecurity.java
@@ -14,7 +14,9 @@ public class WebSphereSecurity {
 	private String clientTrustPassword;
 	
 	@DataBoundConstructor
-	public WebSphereSecurity(String username,String password,String clientKeyFile,String clientKeyPassword,String clientTrustFile,String clientTrustPassword) {
+	public WebSphereSecurity(String username, String password,
+			String clientKeyFile, String clientKeyPassword,
+			String clientTrustFile, String clientTrustPassword) {
 		this.username = username;
 		setPassword(password);
 		this.clientKeyFile = clientKeyFile;
@@ -22,41 +24,52 @@ public class WebSphereSecurity {
 		this.clientTrustFile = clientTrustFile;
 		setClientTrustPassword(clientTrustPassword);
 	}
-	
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return Scrambler.descramble(password);
 	}
+
 	public void setPassword(String password) {
 		this.password = Scrambler.scramble(password);
 	}
+
 	public String getClientKeyFile() {
 		return clientKeyFile;
 	}
+
 	public void setClientKeyFile(String clientKeyFile) {
 		this.clientKeyFile = clientKeyFile;
 	}
+
 	public String getClientKeyPassword() {
 		return Scrambler.descramble(clientKeyPassword);
 	}
+
 	public void setClientKeyPassword(String clientKeyPassword) {
 		this.clientKeyPassword = Scrambler.scramble(clientKeyPassword);
 	}
+
 	public String getClientTrustFile() {
 		return clientTrustFile;
 	}
+
 	public void setClientTrustFile(String clientTrustFile) {
 		this.clientTrustFile = clientTrustFile;
 	}
+
 	public String getClientTrustPassword() {
 		return Scrambler.descramble(clientTrustPassword);
 	}
+
 	public void setClientTrustPassword(String clientTrustPassword) {
 		this.clientTrustPassword = Scrambler.scramble(clientTrustPassword);
-	}	
+	}
 }
--- src/main/java/org/jenkinsci/plugins/websphere_deployer/WebSphereTarget.java
+++ src/main/java/org/jenkinsci/plugins/websphere_deployer/WebSphereTarget.java
@@ -11,30 +11,39 @@ public class WebSphereTarget {
 	public String getCell() {
 		return cell;
 	}
+
 	public void setCell(String cell) {
 		this.cell = cell;
 	}
+
 	public String getNode() {
 		return node;
 	}
+
 	public void setNode(String node) {
 		this.node = node;
 	}
+
 	public String getServer() {
 		return server;
 	}
+
 	public void setServer(String server) {
 		this.server = server;
 	}
+
 	public String getCluster() {
 		return cluster;
 	}
+
 	public void setCluster(String cluster) {
 		this.cluster = cluster;
 	}
+
 	public boolean isSelected() {
 		return selected;
 	}
+
 	public void setSelected(boolean selected) {
 		this.selected = selected;
 	}
